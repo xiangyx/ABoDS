bf3efc810b86064667ed6a6559be8e05f804a771
xiangyx <xiangyx7@gmail.com>
2021-09-01 21:55:59 +0200

    env config

--- Chap_1/Intro_to_Python.py
+++ Chap_1/Intro_to_Python.py
@@ -1,2 +1,18 @@
-print("Hello World!")
-print("Something New.")
+## Calculator
+from math import log, exp
+print(log(exp(1)))
+exp(1)
+
+## Variable & Type
+a = 2
+b = 5.0
+x = "Hello World"
+print(a)
+print(b)
+print(x)
+
+c = 2 * a + b
+print(c)
+
+print(type(x))
+print(type(c))
--- Chap_1/Intro_to_R.R
+++ Chap_1/Intro_to_R.R
@@ -1,1 +1,20 @@
-cat("This is the introduction to R programming.")
+
+# Variable & Type -----------------------------------------------------------
+
+a <- 2
+b <- 5.0
+x <- "Hello World"
+
+print(a)
+print(b)
+cat(x, "\n")
+
+c <- a * 2 + b
+print(c)
+
+print(typeof(x))
+print(typeof(c))
+
+for (i in 1:10) {
+    cat("This is ", i, "\n")
+}
