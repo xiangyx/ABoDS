eac044bc0de7414ae0d4e518adbd49b577e8b92e
xiangyx <xiangyx7@gmail.com>
2021-09-02 10:32:17 +0200

    control flows

--- Chap_1/Intro_to_Python.py
+++ Chap_1/Intro_to_Python.py
@@ -16,3 +16,62 @@ print(c)
 
 print(type(x))
 print(type(c))
+
+## Functions
+def fun1(x):
+    return x**x
+print(fun1(2))
+
+def fun2():
+    print("Input and output are not needed here.")
+fun2()
+
+def fun3(x, base = 2):
+    return log(x, base)
+print(f"base {10}: {fun3(10, 10)}")
+print("base {}: {}".format(exp(1).__round__(4), fun3(exp(1), exp(1))))
+print(f"base {2}: {fun3(2, base = 2)}")
+
+def fun4(x):
+    if x >= 0:
+        return x
+    else:
+        return -x
+print(fun4(10))
+print(fun4(-10))
+
+def fun5(x):
+    if x > 0:
+        return "positive"
+    elif x < 0:
+        return "negative"
+    else:
+        return "zero"
+print(fun5(10))
+print(fun5(-10))
+print(fun5(0))
+
+# For loop
+for i in range(1, 4):
+    print(i)
+
+print(type(range(1, 4)))
+
+# While loop
+i = 1
+while i <= 3:
+    print(i)
+    i += 1
+print("-------\n")
+
+# Contibue/Break
+
+for i in range(1, 4):
+    if i == 2: continue
+    print(i)
+
+print("-------\n")
+for i in range(1, 4):
+    if i == 2: break
+    print(i)
+print("-------\n")
--- Chap_1/Intro_to_R.R
+++ Chap_1/Intro_to_R.R
@@ -15,6 +15,68 @@ print(c)
 print(typeof(x))
 print(typeof(c))
 
-for (i in 1:10) {
-    cat("This is ", i, "\n")
+# Functions-----------------------------------------------
+fun1 <- function(x) {
+    x^x
 }
+cat(fun1(2), "\n")
+
+fun2 <- function() {
+    cat("Input and output are not needed here. \n")
+}
+fun2()
+
+fun3 <- function(x, base = 10) {
+    log(x, base)
+}
+cat("base: 10,", fun3(10), "\n")
+cat("base: 2,", fun3(2, 2), "\n")
+cat("base:", exp(1), ",", fun3(exp(1), exp(1)), "\n")
+cat("base: 2,", fun3(base = 2, 2), "\n")
+
+fun4 <- function(x) {
+    if (x >= 0) 
+        return(x)
+    else
+        return(-x) 
+}
+print(fun4(10))
+print(fun4(-10))
+
+fun5 <- function(x) {
+    if (x > 0)
+        return("positive")
+    else if (x < 0)
+        return("negative")
+    else 
+        return("zero")
+}
+cat(fun5(10), "\n")
+cat(fun5(-10), "\n")
+cat(fun5(0), "\n")
+
+# For loop
+for(i in 1:3) {
+    print(i)
+}
+print(typeof(1:3))
+
+# While loop
+i <- 1
+while(i <= 3) {
+    print(i)
+    i <- i + 1
+}
+
+# Next/Break
+cat("-------\n")
+for(i in 1:3) {
+    if (i == 2) next
+    print(i)
+}
+cat("-------\n")
+for(i in 1:3) {
+    if (i == 2) break
+    print(i)
+}
+cat("-------\n")
